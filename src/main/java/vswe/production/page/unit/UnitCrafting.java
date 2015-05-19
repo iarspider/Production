94e6cc3f29c66560544e464642d7c07f54e3bd06
iarspider <iarspider@gmail.com>
2015-05-19 14:56:31 +0300

    Update UnitCrafting.java
    Only fire event if a player is valid

--- src/main/java/vswe/production/page/unit/UnitCrafting.java
+++ src/main/java/vswe/production/page/unit/UnitCrafting.java
@@ -103,14 +103,16 @@ public class UnitCrafting extends Unit {
         }
         Item item = itemStack.getItem();
 
-        try {
-            FMLCommonHandler.instance().firePlayerCraftingEvent(player, itemStack, inventoryCrafting);
-        }catch (Exception ex) {
-            ex.printStackTrace();
-        }
         if(player != null) {
+            try {
+                FMLCommonHandler.instance().firePlayerCraftingEvent(player, itemStack, inventoryCrafting);
+            }catch (Exception ex) {
+                ex.printStackTrace();
+            }
+
             player.addStat(StatList.objectCraftStats[Item.getIdFromItem(item)], itemStack.stackSize);
         }
+        
         try {
             item.onCreated(itemStack, table.getWorldObj(), player);
         }catch (Exception ex) {
